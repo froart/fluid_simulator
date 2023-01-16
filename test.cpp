4f74b8e6b93aa00f6d17041052f051150412095e
froart <artemiusxyz@gmail.com>
2023-01-16 22:35:30 +0100

    Velocity sources added to form a circular flow

--- test.cpp
+++ test.cpp
@@ -1,16 +1,29 @@
 #include "opengl_setup.hpp"
 #include "fluid_dynamics.hpp"
 #include <iostream>
+#include <omp.h>
 
 using namespace std;
 
 int width = 200;
 int height = 200;
 float* image = new float[width * height];
-Fluid f(image, width, height, 0.1, 0.0, 0.0);			
+Fluid f(image, width, height, 0.1, 0.0, 100.01);			
+int brush_size = 10;
 
 void loop_code() {
 	// post fluid code here
+	float speed = 10;
+ 	for(int k = -5; k < 5; ++k) {
+//	f.addVelocity(width/2+k, height/2+5, 0, speed); 
+//	f.addVelocity(width/2+k, height/2-5, 0, -speed); 
+//	f.addVelocity(width/2-5, height/2+k, -speed, 0); 
+//	f.addVelocity(width/2+5, height/2+k, speed, 0); 
+  	f.addVelocity(width/3+k, height/3, 0, speed); 
+  	f.addVelocity(2*width/3, height/3+k, -speed, 0); 
+  	f.addVelocity(width/3, 2*height/3+k, speed, 0); 
+  	f.addVelocity(2*width/3+k, 2*height/3, 0, -speed); 
+  }
 	f.evaluate(5);	
 	return;
 }
@@ -24,10 +37,9 @@ void mouse(int button, int state, int x, int y){
 	if(button == GLUT_RIGHT_BUTTON)
 		std::fill_n(image, width*height, 0);
 	if(button == GLUT_LEFT_BUTTON) {
-		for(int i = -3; i <= 3; ++i)
-			for(int j = -3; j <= 3; ++j)
-				f.addSource(width/2+i, height/2+j, 1.5);
-	 	f.addVelocity(width/2, height/2, 10*(x-width/2), 10*(height-y-height/2)); 
+		for(int i = -brush_size/2; i <= brush_size/2; ++i)
+			for(int j = -brush_size/2; j <= brush_size/2; ++j)
+				f.addSource(x+i, height-y+j, 1.0);
 	}
 }
 
