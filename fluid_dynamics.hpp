5ea3c37db0b38d8149349e48b57e82edb56fe5c8
froart <artemiusxyz@gmail.com>
2023-01-19 11:15:20 +0100

    Minor fixes

--- fluid_dynamics.hpp
+++ fluid_dynamics.hpp
@@ -8,6 +8,7 @@ class Fluid {
 		float dt_;
 		float viscosity_;
 		float diffusion_rate_;
+		int iterations_;
 		float* vx_;
 		float* vy_;
 		float* vx0_;
@@ -16,15 +17,15 @@ class Fluid {
 		float* s_;
 		void setBoundary(int, float*);
 		void solveLinear(int, float*, float*, float, float, int);
-		void diffuse(int, float*, float*, int);
-		void project(float*, float*, float*, float*, int);
+		void diffuse(int, float*, float*);
+		void project(float*, float*, float*, float*);
 		void advect(int, float*, float*, float*, float*);
 	public:
-		Fluid(float*, int, int, float, float, float);
+		Fluid(float*, int, int, float, float, float, int);
 		~Fluid();
 		void addSource(int, int, float);
 		void addVelocity(int, int, float, float);
-		void evaluate(int);
+		void evaluate();
 };
 
 #endif // FLUID_DYNAMICS_HPP_
